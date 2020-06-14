571233521780cee32da8a2dfbdceb234ca6fb023
石进 <shi.jin@outlook.com>
2020-06-14 15:48:36 +0800

    增加保存 Excel 表的功能

--- wingide/quanttest.py
+++ wingide/quanttest.py
@@ -7,15 +7,19 @@ import easytrader
 import pandas as pd
 
 user = easytrader.use("ht_client")
+
 # 手动登入客户端，自动登入采用明文密码不安全
 user.connect()
+user.refresh()
 
-bill_2017 = user.get_exchangebill_year(2017)
-# bill_2020 = user.get_exchangebill_year(2020)
+bill_2019 = user.get_exchangebill_year(2019)
 
 # 将字典转换为 DataFrame
-df = pd.DataFrame(bill_2017)
+df = pd.DataFrame(bill_2019)
 
 # 去除不需要的行和列
 
 # DataFrame 存储为 Excel 表格
+wrxl = pd.ExcelWriter("交易清单 2019.xlsx")
+df.to_excel(wrxl, '2019 年', index=False)
+wrxl.save()
